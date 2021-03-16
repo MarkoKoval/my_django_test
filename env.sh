62027c112efd97fca2ff617fc11e04b0ff25ccdd
MarkoKoval <mark.koval.ua@gmail.com>
2021-03-16 14:42:27 +0200

    init commit

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export SECRET_KEY='(u=km3!s62o9##*2fxlftw851o+8vzd4%tx5r$#^box6t3r6!0'
+export DEBUG=True
+export ALLOWED_HOSTS=*
+export DB_HOST=127.0.0.1
+export DB_NAME=django_test_db
+export DB_PORT=5432
+export DB_USER=postgres
+export DB_PASS=markkoval123
